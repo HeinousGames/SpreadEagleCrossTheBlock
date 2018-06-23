1be390ae5d6fe829bf09f0e431fb15442a31ffc8
ministeve412 <ministeve412@gmail.com>
2018-06-22 22:10:38 -0700

    refactoring button code to match start screen

--- core/src/net/heinousgames/game/spreadeaglecrosstheblock/RecapScreen.java
+++ core/src/net/heinousgames/game/spreadeaglecrosstheblock/RecapScreen.java
@@ -2,33 +2,30 @@ package net.heinousgames.game.spreadeaglecrosstheblock;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /**
  * Created by Steve on 11/26/2016
  */
-class RecapScreen implements Screen, InputProcessor {
+class RecapScreen implements Screen {
 
     private final SpreadEagles game;
-    private int record;
+    private int highScore;
     private OrthographicCamera camera;
     private Stage recapStage;
 
-    RecapScreen(SpreadEagles game) {
+    RecapScreen(final SpreadEagles game) {
         this.game = game;
-
-        Preferences prefs = Gdx.app.getPreferences("userPrefs");
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1334, 750);
@@ -43,29 +40,41 @@ class RecapScreen implements Screen, InputProcessor {
         recapStage = new Stage(new ScreenViewport());
         recapStage.getViewport().setCamera(camera);
 
-        Image startOverButton = new Image(game.buttonTexture);
-        startOverButton.setName("startOver");
-        startOverButton.setPosition(780, 350.5f);
-        startOverButton.setHeight(120);
-        startOverButton.setWidth(420);
-        recapStage.addActor(startOverButton);
+        Button btnPlayAgain = new TextButton("Restart", game.style);
+        btnPlayAgain.setPosition(517, 350);
+        btnPlayAgain.setSize(300, 100);
+        btnPlayAgain.setSkin(game.buttonSkin);
+        btnPlayAgain.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                dispose();
+                game.setScreen(new MainLevel(game));
+            }
+        });
+        recapStage.addActor(btnPlayAgain);
 
         if (Gdx.app.getType() != Application.ApplicationType.WebGL) {
-            Image quitButton = new Image(game.buttonTexture);
-            quitButton.setName("quit");
-            quitButton.setPosition(1020, 100);
-            quitButton.setHeight(120);
-            quitButton.setWidth(180);
-            recapStage.addActor(quitButton);
+            Button btnQuit = new TextButton("Quit", game.style);
+            btnQuit.setPosition(517, 150);
+            btnQuit.setSize(300, 100);
+            btnQuit.setSkin(game.buttonSkin);
+            btnQuit.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    dispose();
+                    Gdx.app.exit();
+                }
+            });
+            recapStage.addActor(btnQuit);
         }
 
-        Gdx.input.setInputProcessor(this);
+        Gdx.input.setInputProcessor(recapStage);
 
-        record = prefs.getInteger("highScore");
-        if (game.score > record) {
-            prefs.putInteger("highScore", game.score);
-            prefs.flush();
-            record = prefs.getInteger("highScore");
+        highScore = game.prefs.getInteger("highScore");
+        if (game.score > highScore) {
+            game.prefs.putInteger("highScore", game.score);
+            game.prefs.flush();
+            highScore = game.prefs.getInteger("highScore");
         }
     }
 
@@ -78,16 +87,14 @@ class RecapScreen implements Screen, InputProcessor {
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        game.shapeRenderer.rect(0, 0, 1334, 750, game.VULTURE_BLUE, game.VULTURE_BLUE, Color.BLACK, Color.BLACK);
+        game.shapeRenderer.rect(0, 0, 1334, 750, game.vultureBlue, game.vultureBlue, Color.BLACK, Color.BLACK);
         game.shapeRenderer.end();
 
         recapStage.draw();
 
         game.batch.begin();
-        game.fontExmilitary100.draw(game.batch, "Quit", 1030, 210);
-        game.fontExmilitary100.draw(game.batch, "Play Again", 790, 465);
-        game.fontExmilitary100.draw(game.batch, "Score: " + game.score, 100, 700);
-        game.fontExmilitary100.draw(game.batch, "High Score: " + record, 100, 600);
+        game.fontExmilitary100.draw(game.batch, "Score: " + game.score, 400, 700);
+        game.fontExmilitary100.draw(game.batch, "High Score: " + highScore, 400, 600);
         game.batch.end();
 
         camera.update();
@@ -118,56 +125,4 @@ class RecapScreen implements Screen, InputProcessor {
         recapStage.dispose();
     }
 
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        Vector2 coord = recapStage.screenToStageCoordinates(new Vector2((float)screenX, (float)screenY));
-        Actor hitActor = recapStage.hit(coord.x, coord.y, false);
-
-        if (hitActor != null) {
-            if (hitActor.getName().equals("startOver")) {
-                dispose();
-                game.setScreen(new MainLevel(game));
-            } else if (hitActor.getName().equals("quit")) {
-                dispose();
-                Gdx.app.exit();
-            }
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
 }
