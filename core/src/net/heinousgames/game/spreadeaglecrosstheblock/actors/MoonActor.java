1b677a24f2bea631c366e2d546f2a0c34f02e812
ministeve412 <ministeve412@gmail.com>
2018-06-24 12:01:23 -0700

    updating to reflect cloud actor

--- core/src/net/heinousgames/game/spreadeaglecrosstheblock/actors/MoonActor.java
+++ core/src/net/heinousgames/game/spreadeaglecrosstheblock/actors/MoonActor.java
@@ -11,15 +11,11 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class MoonActor extends Actor {
 
     private Texture texture;
+    public float speed;
 
-    public void setMOON_SPEED(float MOON_SPEED) {
-        this.MOON_SPEED = MOON_SPEED;
-    }
-
-    private float MOON_SPEED = 0.5f;
-
-    public MoonActor() {
-        texture = new Texture(Gdx.files.internal("gfx/moon.png"));
+    public MoonActor(Texture texture) {
+        this.texture = texture;
+        this.speed = 0.5f;
         setX(-3);
     }
 
@@ -28,7 +24,7 @@ public class MoonActor extends Actor {
         batch.draw(texture, getX(), 8.2f, 2.5f, 2.5f);
 
         // 0.05 places behind building
-        setX(getX() + (MOON_SPEED * Gdx.graphics.getDeltaTime()));
+        setX(getX() + (speed * Gdx.graphics.getDeltaTime()));
 
         if (getX() >= 20) {
             setX(-3);
