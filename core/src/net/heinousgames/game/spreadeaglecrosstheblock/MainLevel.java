a54632383cd6d8732e8deb62fd4ffe3447ab9c69
ministeve412 <ministeve412@gmail.com>
2018-06-24 11:20:42 -0700

    setting start point back to original

--- core/src/net/heinousgames/game/spreadeaglecrosstheblock/MainLevel.java
+++ core/src/net/heinousgames/game/spreadeaglecrosstheblock/MainLevel.java
@@ -203,7 +203,7 @@ class MainLevel implements Screen, InputProcessor {
         // create the levelCamera to show 20x11 world units
         levelCamera = new OrthographicCamera(20, 11);
         // position the levelCamera to the world units
-        levelCamera.position.x = 40; //802;
+        levelCamera.position.x = 802;
         levelCamera.position.y = 5.5f;
 
         tileStage = new Stage(new ScreenViewport());
