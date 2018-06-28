dff077f2c20d24fb45a7a6262a23eeec1ba9bfda
ministeve412 <ministeve412@gmail.com>
2018-06-27 19:38:26 -0700

    adding icons for package distribution

--- desktop/src/net/heinousgames/game/spreadeaglecrosstheblock/desktop/DesktopLauncher.java
+++ desktop/src/net/heinousgames/game/spreadeaglecrosstheblock/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package net.heinousgames.game.spreadeaglecrosstheblock.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -11,6 +12,15 @@ public class DesktopLauncher {
         config.title = "Spread Eagle Cross the Block";
 		config.width = 1334;
 		config.height = 750;
+		config.addIcon("gfx/moon_mac_icon.png", Files.FileType.Internal);
+		config.addIcon("gfx/moon_desktop_icon.png", Files.FileType.Internal);
+		config.addIcon("gfx/moon_desktop_icon16x.png", Files.FileType.Internal);
+
+		// make the game run windowed and borderless
+//		System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+//		config.width = (int) (LwjglApplicationConfiguration.getDesktopDisplayMode().width);
+//		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
+
 		new LwjglApplication(new SpreadEagles(new DesktopDateFormatter()), config);
 	}
 }
